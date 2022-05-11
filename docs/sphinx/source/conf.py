430b42c584f078dfd26c5354eb966aa5078d2333
ScriptExec <scriptexec.contact@gmail.com>
2022-05-11 18:06:20 +0200

    Updated config

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -1,4 +1,5 @@
-import subprocess
+import subprocess, shutil
+shutil.rmtree('../../doxygen/build/html')
 subprocess.call('make clean', shell=True)
-# subprocess.call('cd ../../doxygen ; doxygen', shell=True)
+subprocess.call('cd ../../doxygen ; doxygen', shell=True)
 html_extra_path = ['../../doxygen/build/html']
